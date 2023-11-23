5b0b5d8aa0e7bd6134f201145c459fc2b01ee514
Dyumell <sorbet0231@gmail.com>
2023-11-23 22:05:21 +0900

    회원정보검색기능80%완료, 나머지 20%는 사용내역조회와 계정 삭제로 피시사용현황등 선행작업이 필요

--- login/SearchAccount.Designer.cs
+++ login/SearchAccount.Designer.cs
@@ -28,55 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtSearchUserID = new System.Windows.Forms.TextBox();
+            this.txtSearchUserName = new System.Windows.Forms.TextBox();
+            this.txtSearchUserMail = new System.Windows.Forms.TextBox();
+            this.txtSearchUserPhoneNum = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.SearchUserID = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.listViewSearchResult = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SearchUserNameBtn = new System.Windows.Forms.Button();
+            this.SearchUserPhoneBtn = new System.Windows.Forms.Button();
+            this.SearchUserMailBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtSearchUserID
             // 
-            this.textBox1.Font = new System.Drawing.Font("굴림", 16F);
-            this.textBox1.Location = new System.Drawing.Point(293, 11);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(256, 38);
-            this.textBox1.TabIndex = 0;
+            this.txtSearchUserID.Font = new System.Drawing.Font("굴림", 16F);
+            this.txtSearchUserID.Location = new System.Drawing.Point(293, 11);
+            this.txtSearchUserID.Name = "txtSearchUserID";
+            this.txtSearchUserID.Size = new System.Drawing.Size(206, 38);
+            this.txtSearchUserID.TabIndex = 0;
             // 
-            // textBox2
+            // txtSearchUserName
             // 
-            this.textBox2.Font = new System.Drawing.Font("굴림", 16F);
-            this.textBox2.Location = new System.Drawing.Point(293, 77);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(256, 38);
-            this.textBox2.TabIndex = 1;
+            this.txtSearchUserName.Font = new System.Drawing.Font("굴림", 16F);
+            this.txtSearchUserName.Location = new System.Drawing.Point(293, 77);
+            this.txtSearchUserName.Name = "txtSearchUserName";
+            this.txtSearchUserName.Size = new System.Drawing.Size(206, 38);
+            this.txtSearchUserName.TabIndex = 1;
             // 
-            // textBox3
+            // txtSearchUserMail
             // 
-            this.textBox3.Font = new System.Drawing.Font("굴림", 16F);
-            this.textBox3.Location = new System.Drawing.Point(293, 201);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(256, 38);
-            this.textBox3.TabIndex = 3;
+            this.txtSearchUserMail.Font = new System.Drawing.Font("굴림", 16F);
+            this.txtSearchUserMail.Location = new System.Drawing.Point(293, 201);
+            this.txtSearchUserMail.Name = "txtSearchUserMail";
+            this.txtSearchUserMail.Size = new System.Drawing.Size(206, 38);
+            this.txtSearchUserMail.TabIndex = 3;
             // 
-            // textBox4
+            // txtSearchUserPhoneNum
             // 
-            this.textBox4.Font = new System.Drawing.Font("굴림", 16F);
-            this.textBox4.Location = new System.Drawing.Point(293, 140);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(256, 38);
-            this.textBox4.TabIndex = 2;
+            this.txtSearchUserPhoneNum.Font = new System.Drawing.Font("굴림", 16F);
+            this.txtSearchUserPhoneNum.Location = new System.Drawing.Point(293, 140);
+            this.txtSearchUserPhoneNum.Name = "txtSearchUserPhoneNum";
+            this.txtSearchUserPhoneNum.Size = new System.Drawing.Size(206, 38);
+            this.txtSearchUserPhoneNum.TabIndex = 2;
             // 
             // label1
             // 
@@ -97,7 +101,6 @@
             this.label2.Size = new System.Drawing.Size(167, 29);
             this.label2.TabIndex = 5;
             this.label2.Text = "사용자 성함";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -119,40 +122,44 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "이메일";
             // 
-            // button1
+            // SearchUserID
             // 
-            this.button1.Location = new System.Drawing.Point(426, 506);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 56);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "검색";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.SearchUserID.Location = new System.Drawing.Point(505, 15);
+            this.SearchUserID.Name = "SearchUserID";
+            this.SearchUserID.Size = new System.Drawing.Size(71, 34);
+            this.SearchUserID.TabIndex = 9;
+            this.SearchUserID.Text = "검색";
+            this.SearchUserID.UseVisualStyleBackColor = true;
+            this.SearchUserID.Click += new System.EventHandler(this.SearchUserID_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(293, 506);
+            this.button2.Location = new System.Drawing.Point(441, 496);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(123, 56);
             this.button2.TabIndex = 10;
-            this.button2.Text = "button2";
+            this.button2.Text = "사용내역 조회";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // listView1
+            // listViewSearchResult
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listViewSearchResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3,
             this.columnHeader4,
             this.columnHeader5});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(31, 279);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(533, 202);
-            this.listView1.TabIndex = 11;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listViewSearchResult.FullRowSelect = true;
+            this.listViewSearchResult.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewSearchResult.HideSelection = false;
+            this.listViewSearchResult.Location = new System.Drawing.Point(31, 279);
+            this.listViewSearchResult.Name = "listViewSearchResult";
+            this.listViewSearchResult.Size = new System.Drawing.Size(533, 202);
+            this.listViewSearchResult.TabIndex = 11;
+            this.listViewSearchResult.UseCompatibleStateImageBehavior = false;
+            this.listViewSearchResult.View = System.Windows.Forms.View.Details;
+            this.listViewSearchResult.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // columnHeader1
             // 
@@ -178,22 +185,65 @@
             this.columnHeader5.Text = "비밀번호";
             this.columnHeader5.Width = 268;
             // 
+            // SearchUserNameBtn
+            // 
+            this.SearchUserNameBtn.Location = new System.Drawing.Point(505, 80);
+            this.SearchUserNameBtn.Name = "SearchUserNameBtn";
+            this.SearchUserNameBtn.Size = new System.Drawing.Size(71, 34);
+            this.SearchUserNameBtn.TabIndex = 12;
+            this.SearchUserNameBtn.Text = "검색";
+            this.SearchUserNameBtn.UseVisualStyleBackColor = true;
+            this.SearchUserNameBtn.Click += new System.EventHandler(this.SearchUserNameBtn_Click);
+            // 
+            // SearchUserPhoneBtn
+            // 
+            this.SearchUserPhoneBtn.Location = new System.Drawing.Point(505, 144);
+            this.SearchUserPhoneBtn.Name = "SearchUserPhoneBtn";
+            this.SearchUserPhoneBtn.Size = new System.Drawing.Size(71, 34);
+            this.SearchUserPhoneBtn.TabIndex = 13;
+            this.SearchUserPhoneBtn.Text = "검색";
+            this.SearchUserPhoneBtn.UseVisualStyleBackColor = true;
+            this.SearchUserPhoneBtn.Click += new System.EventHandler(this.SearchUserPhoneBtn_Click);
+            // 
+            // SearchUserMailBtn
+            // 
+            this.SearchUserMailBtn.Location = new System.Drawing.Point(505, 204);
+            this.SearchUserMailBtn.Name = "SearchUserMailBtn";
+            this.SearchUserMailBtn.Size = new System.Drawing.Size(71, 34);
+            this.SearchUserMailBtn.TabIndex = 14;
+            this.SearchUserMailBtn.Text = "검색";
+            this.SearchUserMailBtn.UseVisualStyleBackColor = true;
+            this.SearchUserMailBtn.Click += new System.EventHandler(this.SearchUserMailBtn_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 496);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 56);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "계정삭제";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // SearchAccount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(606, 574);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.SearchUserMailBtn);
+            this.Controls.Add(this.SearchUserPhoneBtn);
+            this.Controls.Add(this.SearchUserNameBtn);
+            this.Controls.Add(this.listViewSearchResult);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SearchUserID);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtSearchUserMail);
+            this.Controls.Add(this.txtSearchUserPhoneNum);
+            this.Controls.Add(this.txtSearchUserName);
+            this.Controls.Add(this.txtSearchUserID);
             this.Name = "SearchAccount";
             this.Text = "SearchAccount";
             this.ResumeLayout(false);
@@ -203,21 +253,25 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtSearchUserID;
+        private System.Windows.Forms.TextBox txtSearchUserName;
+        private System.Windows.Forms.TextBox txtSearchUserMail;
+        private System.Windows.Forms.TextBox txtSearchUserPhoneNum;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SearchUserID;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listViewSearchResult;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button SearchUserNameBtn;
+        private System.Windows.Forms.Button SearchUserPhoneBtn;
+        private System.Windows.Forms.Button SearchUserMailBtn;
+        private System.Windows.Forms.Button button4;
     }
 }
--- login/SearchAccount.cs
+++ login/SearchAccount.cs
@@ -19,6 +19,9 @@ namespace login
         OracleCommandBuilder myCommandBuilder;
         DataSet DS;
 
+        DataTable resultUserIDTable; // 검색버튼 4개에 의해 자주 쓰일 것
+        DataRow[] resultRows; // 검색버튼 4개에 의해 자주 쓰일 것
+
         public SearchAccount()
         {
             DB_Open();
@@ -26,6 +29,7 @@ namespace login
         }
 
         private void DB_Open() // DB연결
+
         {
             try
             {
@@ -45,28 +49,119 @@ namespace login
             }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+
+        private void SearchUserID_Click(object sender, EventArgs e) // 아이디로 검색하기 버튼
         {
+            listViewSearchResult.Items.Clear(); // 리스트뷰 초기화
+            DS.Clear(); // DS 초기화
+            DBAdapter.Fill(DS, "JoinedData"); // DS에 쿼리한결과담음
+            resultUserIDTable = DS.Tables["JoinedData"]; // 복사본을 DataTable 변수에 할당
+            resultRows // 할당된 테이블에서 쿼리를 적용하기 위한 DataRow변수
+            = resultUserIDTable.Select("user_id like '%" + txtSearchUserID.Text + "%'"); // DataRow[] 를 초기화할때는 Array.Clear(resultRow, 0, resultRow.length) 사용
 
-        }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            DS.Clear();
-            DBAdapter.Fill(DS, "JoinedData");
-            DataTable resultTable = DS.Tables["JoinedData"];
-            DataRowCollection rows = resultTable.Rows;
 
-            foreach (DataRow dr in rows)
+            foreach (DataRow dr in resultRows) // foreach문을 이용해서, 쿼리의 결과를 리스트뷰에 출력
             {
-                ListViewItem item = new ListViewItem(dr[0].ToString());
-                for (int i = 1; i < resultTable.Columns.Count; i++)
-                {
+                ListViewItem item = new ListViewItem(dr[0].ToString()); // 첫 번째 열을 첫 번째 서브아이템으로 추가
+
+                // 나머지 열에 대해 서브아이템 추가
+                for (int i = 1; i < resultUserIDTable.Columns.Count; i++) // 만약 위처럼 첫번째열을 첫번째 서브아이템으로 추가하는 방식을 
+                {                                                         // 사용하지 않을 시 리스트뷰가 전부 한칸씩 밀리게된다.
                     item.SubItems.Add(dr[i].ToString());
                 }
-                listView1.Items.Add(item);
+
+                listViewSearchResult.Items.Add(item);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewSearchResult.SelectedIndices.Count > 0)
+            {
+                int selectedIndex = listViewSearchResult.SelectedIndices[0]; // 첫 번째 선택된 아이템의 인덱스
+                ListViewItem selectedItem = listViewSearchResult.Items[selectedIndex]; // 선택된 아이템
+                string column1Value = selectedItem.SubItems[0].Text;
+
+                MessageBox.Show($"Selected: Column1 - {column1Value}");
 
             }
         }
+
+        private void SearchUserNameBtn_Click(object sender, EventArgs e)
+        {
+            listViewSearchResult.Items.Clear();
+            DS.Clear();
+            DBAdapter.Fill(DS, "JoinedData");
+            resultUserIDTable = DS.Tables["JoinedData"];
+            resultRows
+    = resultUserIDTable.Select("user_name like '%" + txtSearchUserName.Text + "%'");
+
+            foreach (DataRow dr in resultRows)
+            {
+                ListViewItem item = new ListViewItem(dr[0].ToString()); // 첫 번째 열을 첫 번째 서브아이템으로 추가
+
+                // 나머지 열에 대해 서브아이템 추가
+                for (int i = 1; i < resultUserIDTable.Columns.Count; i++) // 만약 위처럼 첫번째열을 첫번째 서브아이템으로 추가하는 방식을 
+                {                                                         // 사용하지 않을 시 리스트뷰가 전부 한칸씩 밀리게된다.
+                    item.SubItems.Add(dr[i].ToString());
+                }
+
+                listViewSearchResult.Items.Add(item);
+            }
+        }
+
+        private void SearchUserPhoneBtn_Click(object sender, EventArgs e)
+        {
+            listViewSearchResult.Items.Clear();
+            DS.Clear();
+            DBAdapter.Fill(DS, "JoinedData");
+            resultUserIDTable = DS.Tables["JoinedData"];
+            resultRows
+    = resultUserIDTable.Select("user_phone_num like '%" + txtSearchUserPhoneNum.Text + "%'");
+
+            foreach (DataRow dr in resultRows)
+            {
+                ListViewItem item = new ListViewItem(dr[0].ToString()); // 첫 번째 열을 첫 번째 서브아이템으로 추가
+
+                // 나머지 열에 대해 서브아이템 추가
+                for (int i = 1; i < resultUserIDTable.Columns.Count; i++)
+                {
+                    item.SubItems.Add(dr[i].ToString());
+                }
+
+                listViewSearchResult.Items.Add(item);
+
+            }
+        }
+
+        private void SearchUserMailBtn_Click(object sender, EventArgs e)
+        {
+            listViewSearchResult.Items.Clear();
+            DS.Clear();
+            DBAdapter.Fill(DS, "JoinedData");
+            resultUserIDTable = DS.Tables["JoinedData"];
+            resultRows
+                        = resultUserIDTable.Select("user_email like '%" + txtSearchUserMail.Text + "%'");
+                foreach (DataRow dr in resultRows)
+                {
+                    ListViewItem item = new ListViewItem(dr[0].ToString()); // 첫 번째 열을 첫 번째 서브아이템으로 추가
+
+                    // 나머지 열에 대해 서브아이템 추가
+                    for (int i = 1; i < resultUserIDTable.Columns.Count; i++)
+                    {
+                        item.SubItems.Add(dr[i].ToString());
+                    }
+
+                    listViewSearchResult.Items.Add(item);
+
+                }
+            }
+
+        private void button2_Click(object sender, EventArgs e) //  사용내역조회버튼 -선행작업필요: 좌석관리디스플레이, 피시정보등
+        {
+
+        }
     }
-}
+    }
+
